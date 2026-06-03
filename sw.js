6114ada31ca0cf5c8c44ecb66a11f80a29eadb21
Khouini Yacine <khouini.medyacine@gmail.com>
2026-06-03 09:19:46 +0100

    Add PWA support with manifest, service worker, and prayer time features

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "saydaliati-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./logo.svg"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
